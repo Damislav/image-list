6a5e93eed75beb8ee2dd01888c78f10c12ab9615
Damislav <idamjanovic28@gmail.com>
2021-04-09 21:05:31 +0200

    #

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import axios from "axios";
 import SearchInput from "./components/SearchInput";
 class App extends Component {
-  state = { items: [] };
+  state = {
+    images: [],
+  };
 
-  async onSearchSubmit(entry) {
-    console.log(entry);
+  onSearchSubmit = async (entry) => {
+    // when i write sometihng in input it gets put to entry in link
     const response = await axios.get(
       `https://pixabay.com/api/?key=21085798-195cd403946ce8bf293dce4af&q=${entry}&image_type=photo`
     );
-    console.log(response);
-  }
+    console.log(response.data.hits);
+    this.setState({
+      images: response.data.hits,
+    });
+  };
   // it will log searchinput entry aka whatever you write
   //this function expects parameter called entry and logs it
 
@@ -19,6 +24,7 @@ class App extends Component {
       <div className="ui container" style={{ marginTop: "30px" }}>
         {/* ¸pass function submit ion search input */}
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        We have {this.state.images.length}
       </div>
     );
   }
